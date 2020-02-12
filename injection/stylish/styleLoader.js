7e4e791dfef5ce7dba50dae8dcf1bdf4f79e9f3f
Michael Higginson <Michael@michaels-mbp-2.home>
2020-02-12 16:21:19 +0000

    old crappy code

---
+++ injection/stylish/styleLoader.js
@@ -0,0 +1,34 @@
+// $.getScript('https://dl.dropboxusercontent.com/s/m9c0xfu5weds98b/TaveAll%202.js',function(){});
+// $.getScript('https://dl.dropboxusercontent.com/s/xzqv9e3g69vf572/styleLoader.js',function(){});
+
+const styleLoader = styleMap => {
+  styleMap.forEach(pair => {
+    const regex = pair[0]
+    const link = pair[1]
+    if( regex.test( window.location.href )) {
+        const style = document.createElement('link')
+        style.type = 'text/css'
+        style.href = convertDropBoxLink(link)
+        style.rel = 'stylesheet'
+        document.getElementsByTagName('head')[0].appendChild(style)
+    }
+  })
+}
+
+const convertDropBoxLink = link => {
+  const regex = /w+\.\w+/
+  return link.replace(regex, "dl.dropboxusercontent")
+}
+
+styleLoader([
+  [/forms\/response\/edit/, "https://www.dropbox.com/s/br1wztkektlrssy/questionnaire.css?dl=0"],
+  [/jobs\/(edit)|(forms\/response\/edit)|(orders\/new)/, "https://www.dropbox.com/s/4ou7ytoh13v9r3c/missingPopup.css?dl=0"],
+  [/tave/, "https://www.dropbox.com/s/yuuuphroc2xvfgd/due.css?dl=0"],
+  [/tave/, "https://www.dropbox.com/s/yuuuphroc2xvfgd/due.css?dl=0"],
+  [/tave/, "https://www.dropbox.com/s/gp9br6efer57fvv/smallNav.css?dl=0"],
+  [/tave/, "https://www.dropbox.com/s/c9jodd0exqo1f4s/forceWhiteText.css?dl=0"],
+  [/app\/tasks/,"https://www.dropbox.com/s/ntgxkbvg0j1v0dk/taskFliter.css?dl=0"],
+  [/calendar\/month/,"https://www.dropbox.com/s/gmin5sf0yx9jg9y/expandBlocks.css?dl=0"],
+  [/calendar/,"https://www.dropbox.com/s/72i1kc7ulv7a1aj/largeCal.css?dl=0"],
+  [/calendar\/month/,"https://www.dropbox.com/s/72i1kc7ulv7a1aj/largeCal2.css?dl=0"],
+])
---
+++ native/mySlider/mySlider.js
@@ -0,0 +1,65 @@
+const sliders = document.getElementsByClassName("slider")
+
+const initSliders = function(slider) {
+
+  /*------------ Would rather move this stuff out of here start --------------*/
+
+  const slides = slider.getElementsByClassName("slides")
+  const slidesArr = Array.prototype.slice.call(slides)
+  const next = slider.getElementsByClassName("next")[0]
+  const numberNavBox = document.createElement("div")
+  numberNavBox.className = "navNumbers"
+  const numbers = numberNavBox.children
+  let currentSlide = 1
+
+  const changeSlide = n => {
+    slides[currentSlide-1].style.display = 'none'
+    removeActiveNum(currentSlide-1)
+    slides[n-1].style.display = 'block'
+    activeNum(n-1)
+    currentSlide = parseInt(n)
+  }
+
+  const calcSlide = n => {
+    const slide = currentSlide + n
+    if(slide > slides.length) return 1
+    else if(slide < 1) return slides.length
+    else return slide
+  }
+
+  slidesArr.forEach((slide, idx) => {
+    const nthBtn = document.createElement("div")
+    nthBtn.textContent = idx+1
+    nthBtn.classList.add("btn")
+    numberNavBox.appendChild(nthBtn)
+  })
+  slider.insertBefore(numberNavBox, next)
+
+  const activeNum = n => {
+    numbers[n].classList.add("activeNumber")
+  }
+  const removeActiveNum = n => {
+    numbers[n].classList.remove("activeNumber")
+  }
+  activeNum(0)
+
+/*------------- Would rather move this stuff out of here end --------------*/
+
+  slider.addEventListener( "click", e => {
+
+    const potentialNum = e.target.textContent
+    const tagName = e.target.tagName
+
+    if(tagName === "IMG")
+      changeSlide(calcSlide(1))
+    else if(e.target.classList.contains("next"))
+      changeSlide(calcSlide(1))
+    else if(e.target.classList.contains("prev"))
+      changeSlide(calcSlide(-1))
+    else if(!isNaN(potentialNum) && potentialNum !==""){
+      changeSlide(potentialNum)
+    }
+  })
+}
+
+;[].forEach.call(sliders, initSliders)
